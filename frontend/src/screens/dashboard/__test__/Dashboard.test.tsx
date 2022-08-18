c2431c9599f0723a5065a5951de3bc1412a43335
thuyiya <thusitha.4t@gmail.com>
2022-08-18 14:37:18 +0530

    test setup

--- frontend/src/screens/dashboard/__test__/Dashboard.test.tsx
+++ frontend/src/screens/dashboard/__test__/Dashboard.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+import Dashboard from '../Dashboard';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Dashboard />);
+  const linkElement = screen.getByText(/Welcome HR Dashboard/i);
   expect(linkElement).toBeInTheDocument();
 });
