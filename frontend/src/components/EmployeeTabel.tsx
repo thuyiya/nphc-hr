ff160ab4183177e1de07ece28cb5bdaf0abcfd41
thuyiya <thusitha.4t@gmail.com>
2022-08-18 20:49:00 +0530

    salary

--- frontend/src/components/EmployeeTabel.tsx
+++ frontend/src/components/EmployeeTabel.tsx
@@ -1,14 +1,22 @@
 import { Space, Table, Button } from "antd";
 import { DeleteOutlined } from "@ant-design/icons";
+import { ColumnsType } from 'antd/es/table';
 import { EmployeeType } from "../types"
 import { STRINGS } from "../constants";
 
-const columns = [
+interface DataType {
+    key: React.Key;
+    id: string;
+    name: string;
+    login: string;
+    salary: number;
+  }
+
+const columns: ColumnsType<DataType> = [
   {
     title: STRINGS.EMPLOYEE.TABLE_KEY.ID.NAME,
     dataIndex: STRINGS.EMPLOYEE.TABLE_KEY.ID.KEY,
     key: STRINGS.EMPLOYEE.TABLE_KEY.ID.KEY,
-    render: (text: string) => <a>{text}</a>,
   },
   {
     title: STRINGS.EMPLOYEE.TABLE_KEY.NAME.NAME,
@@ -24,7 +32,7 @@ const columns = [
     title: STRINGS.EMPLOYEE.TABLE_KEY.SALARY.NAME,
     key: STRINGS.EMPLOYEE.TABLE_KEY.SALARY.KEY,
     dataIndex: STRINGS.EMPLOYEE.TABLE_KEY.SALARY.KEY,
-    render: (salary: string) => <span>{salary}</span>,
+    sorter: (a, b) => a.salary - b.salary,
   },
   {
     title: "Action",
@@ -43,7 +51,7 @@ const columns = [
 ];
 
 type Props = {
-    data: EmployeeType[]
+    data: any
 }
 
 const EmployeeTabel: React.FC<Props> = ({ data = [] }) => {
