dd32cbd9b78ab18d7c3da8a12739daad29a55245
thuyiya <thusitha.4t@gmail.com>
2022-08-18 15:54:15 +0530

    connect employee views to dashboard

--- frontend/src/screens/dashboard/Dashboard.tsx
+++ frontend/src/screens/dashboard/Dashboard.tsx
@@ -1,11 +1,12 @@
-import DashboardLayout from '../../components/DashboardLayout';
 import './Dashboard.less';
+import DashboardLayout from '../../components/DashboardLayout';
+import UploadEmplyeeWithCsv from '../../components/UploadEmplyeeWithCsv';
 
 function Dashboard() {
   return (
     <div className="App">
       <DashboardLayout>
-        <p>Dashboard HR</p>
+        <UploadEmplyeeWithCsv />
       </DashboardLayout>
     </div>
   );
